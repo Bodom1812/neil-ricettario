521d38c08a044f213be3c39485e90d1a80d8995d
Bodom1812 <102995559+Bodom1812@users.noreply.github.com>
2026-03-13 11:48:04 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://nzfwjeqvuebimyaqqehd.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_AadJaTHhnc7mA8BTa4cBQg_ytSVFcIP';
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
